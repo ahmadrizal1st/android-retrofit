cfe59287c17a87af0da8f1123ff01eb9f4dfa9c1
Ahmad Rizal <ahmaddrizalul@gmail.com>
2025-11-19 09:33:03 +0700

    feat: Initialize Android Retrofit project with basic structure and API integration
    - Added AndroidManifest.xml for application configuration.
    - Created ApiInterface.kt for defining API endpoints.
    - Implemented MainActivity.kt to handle UI and API calls.
    - Defined ResponseData.kt for data model representation.
    - Set up RetrofitInstance.kt for Retrofit configuration.
    - Designed activity_main.xml layout for the main activity.
    - Added drawable resources for app icons.
    - Configured themes and colors for the application.
    - Included backup and data extraction rules XML files.
    - Created unit test ExampleUnitTest.kt for basic functionality.
    - Set up Gradle build files and properties for project dependencies.

---
+++ app/src/main/java/com/android/androidretrofit/ApiInterface.kt
@@ -0,0 +1,9 @@
+package com.android.androidretrofit
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiInterface {
+    @GET("/posts/1")
+    fun getData(): Call<ResponseData>
+}
---
+++ app/src/main/java/com/android/androidretrofit/ResponseData.kt
@@ -0,0 +1,8 @@
+package com.android.androidretrofit
+
+data class ResponseData(
+    val body: String,
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
